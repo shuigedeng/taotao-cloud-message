12048562b3e2b71ddfe520d250544bf9ce9d8574
shuigedeng <981376577@qq.com>
2025-08-30 17:09:21 +0800

    开启2025.10

--- taotao-cloud-message-infrastructure/src/main/java/com/taotao/cloud/message/infrastructure/channels/netty/EnableNettyClient.java
+++ taotao-cloud-message-infrastructure/src/main/java/com/taotao/cloud/message/infrastructure/channels/netty/EnableNettyClient.java
@@ -18,7 +18,9 @@ package com.taotao.cloud.message.infrastructure.channels.netty;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
 
 @Target(ElementType.TYPE)
