3d61358510d944cd45634097940fe5346529cfb1
shuigedeng <981376577@qq.com>
2025-12-23 18:10:31 +0800

    开启2026.01

--- taotao-cloud-message-assembly/src/main/java/com/taotao/cloud/message/TaoTaoCloudMessageApplication.java
+++ taotao-cloud-message-assembly/src/main/java/com/taotao/cloud/message/TaoTaoCloudMessageApplication.java
@@ -71,7 +71,7 @@ public class TaoTaoCloudMessageApplication {
                 .setTtcBanner()
                 .setTtcProfileIfNotExists("dev")
                 .setTtcApplicationProperty("taotao-cloud-message")
-                .setTtcAllowBeanDefinitionOverriding(true)
+                .setTtcAllowBeanDefinitionOverriding(false)
                 .run(args);
     }
 }
