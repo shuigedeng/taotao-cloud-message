d0acf7894db51fedc745c0d5b77315c959901421
shuigedeng <981376577@qq.com>
2026-03-02 21:13:52 +0800

    开启2026.04

--- taotao-cloud-message-application/src/main/generated/com/taotao/cloud/message/application/dto/email/query/EmailQryBuilder.java
+++ taotao-cloud-message-application/src/main/generated/com/taotao/cloud/message/application/dto/email/query/EmailQryBuilder.java
@@ -15,7 +15,7 @@ import javax.annotation.processing.Generated;
  * EmailQryBuilder
  *
  * @author shuigedeng
- * @version 2026.03
+ * @version 2026.04
  * @since 2025-12-19 09:30:45
  */
 @Generated("io.soabase.recordbuilder.core.RecordBuilder")
--- taotao-cloud-message-infrastructure/src/main/java/com/taotao/cloud/message/infrastructure/channels/websockt/spring/admin/listener/NotifyWebsocketEventListener.java
+++ taotao-cloud-message-infrastructure/src/main/java/com/taotao/cloud/message/infrastructure/channels/websockt/spring/admin/listener/NotifyWebsocketEventListener.java
@@ -28,7 +28,7 @@ import org.springframework.scheduling.annotation.Async;
  * NotifyWebsocketEventListener
  *
  * @author shuigedeng
- * @version 2026.03
+ * @version 2026.04
  * @since 2025-12-19 09:30:45
  */
 public class NotifyWebsocketEventListener {
